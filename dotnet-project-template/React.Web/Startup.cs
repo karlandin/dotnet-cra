f1346b89dd79a88a7d54afc472a375d1dab421a2
Karl Andin <7025854+karlandin@users.noreply.github.com>
2019-06-15 15:13:53 +0200

    Removed ClientApp dir and changed client path to ../client-app

--- dotnet-project-template/React.Web/Startup.cs
+++ dotnet-project-template/React.Web/Startup.cs
@@ -25,7 +25,7 @@ namespace React.Web
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/build";
+                configuration.RootPath = "../client-app/build";
             });
         }
 
@@ -56,7 +56,7 @@ namespace React.Web
 
             app.UseSpa(spa =>
             {
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "../client-app";
 
                 if (env.IsDevelopment())
                 {
